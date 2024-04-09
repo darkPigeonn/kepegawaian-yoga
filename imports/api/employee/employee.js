3eb56894184b7062804fcb36e7879588dea1ecec
darkPigeonn <nrio2864@gmail.com>
2024-04-09 12:09:02 +0700

    fixing : update style form pegawai baru

--- imports/api/employee/employee.js
+++ imports/api/employee/employee.js
@@ -1,3 +1,21 @@
 import { Mongo } from "meteor/mongo";
 
 export const Employee = new Mongo.Collection("employees");
+
+export const educationCategory = [
+  { code: 10, label: "SD" },
+  { code: 20, label: "SMP" },
+  { code: 30, label: "SMU" },
+  { code: 31, label: "SMA" },
+  { code: 32, label: "SMK" },
+  { code: 33, label: "STM" },
+  { code: 34, label: "SPG" },
+  { code: 35, label: "SMPS" },
+  { code: 36, label: "SMEA" },
+  { code: 37, label: "D1" },
+  { code: 38, label: "D2" },
+  { code: 39, label: "D3" },
+  { code: 40, label: "S1" },
+  { code: 50, label: "S2" },
+  { code: 60, label: "S3" },
+];
--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,12 +1,11 @@
 // Import client startup through a single index entry point
 
-import './routes.js';
+import "./routes.js";
 import { S3, PutObjectCommand } from "@aws-sdk/client-s3";
 import Swal from "sweetalert2";
 import DecoupledEditor from "../../../public/ckeditor/build/ckeditor";
 import moment from "moment/moment.js";
 // import DecoupledEditor from '@ckeditor/ckeditor5-build-decoupled-document';
-
 
 isEmptyData = function (data) {
   let dataReturn = 0;
@@ -22,28 +21,28 @@ isEmptyData = function (data) {
 };
 
 uploadFiles = async function (uploadData) {
-    const s3Client = new S3({
-      endpoint: Meteor.settings.public.s3.endpoint,
-      region: Meteor.settings.public.s3.region,
-      credentials: {
-        accessKeyId: Meteor.settings.public.s3.credentials.accessKeyId,
-        secretAccessKey: Meteor.settings.public.s3.credentials.secretAccessKey,
-      },
-    });
-    try {
-      await s3Client.send(
-        new PutObjectCommand({
-          Bucket: "imavistatic",
-          Key: uploadData.fileName,
-          Body: uploadData.Body,
-          ACL: "public-read",
-          CacheControl: "no-cache",
-        })
-      );
-      return "https://cdn.imavi.org/" + uploadData.fileName;
-    } catch (err) {
-      console.log("Error " + err);
-    }
+  const s3Client = new S3({
+    endpoint: Meteor.settings.public.s3.endpoint,
+    region: Meteor.settings.public.s3.region,
+    credentials: {
+      accessKeyId: Meteor.settings.public.s3.credentials.accessKeyId,
+      secretAccessKey: Meteor.settings.public.s3.credentials.secretAccessKey,
+    },
+  });
+  try {
+    await s3Client.send(
+      new PutObjectCommand({
+        Bucket: "imavistatic",
+        Key: uploadData.fileName,
+        Body: uploadData.Body,
+        ACL: "public-read",
+        CacheControl: "no-cache",
+      })
+    );
+    return "https://cdn.imavi.org/" + uploadData.fileName;
+  } catch (err) {
+    console.log("Error " + err);
+  }
 };
 
 churchOperationalDays = [
@@ -99,48 +98,48 @@ function isNumber(value) {
 }
 
 function formatPhoneNumber(phoneNumber) {
-// Remove any non-digit characters from the phone number
-const cleanedNumber = phoneNumber.toString().replace(/\D/g, '');
+  // Remove any non-digit characters from the phone number
+  const cleanedNumber = phoneNumber.toString().replace(/\D/g, "");
 
-// Check if the number starts with '0', indicating it's a local number
-if (cleanedNumber.startsWith('0')) {
-  // Remove the leading '0' and prepend the country code '62'
-  const formattedNumber = '62' + cleanedNumber.slice(1);
-  return formattedNumber;
-}
+  // Check if the number starts with '0', indicating it's a local number
+  if (cleanedNumber.startsWith("0")) {
+    // Remove the leading '0' and prepend the country code '62'
+    const formattedNumber = "62" + cleanedNumber.slice(1);
+    return formattedNumber;
+  }
 
-// The number is already in international format, return as is
-return cleanedNumber;
+  // The number is already in international format, return as is
+  return cleanedNumber;
 }
 
 function formatRupiah(angka, prefix) {
-var number_string = angka.replace(/[^,\d]/g, "").toString(),
-  split = number_string.split(","),
-  sisa = split[0].length % 3,
-  rupiah = split[0].substr(0, sisa),
-  ribuan = split[0].substr(sisa).match(/\d{3}/gi);
+  var number_string = angka.replace(/[^,\d]/g, "").toString(),
+    split = number_string.split(","),
+    sisa = split[0].length % 3,
+    rupiah = split[0].substr(0, sisa),
+    ribuan = split[0].substr(sisa).match(/\d{3}/gi);
 
-// tambahkan titik jika yang di input sudah menjadi angka ribuan
-if (ribuan) {
-  separator = sisa ? "." : "";
-  rupiah += separator + ribuan.join(".");
-}
+  // tambahkan titik jika yang di input sudah menjadi angka ribuan
+  if (ribuan) {
+    separator = sisa ? "." : "";
+    rupiah += separator + ribuan.join(".");
+  }
 
-rupiah = split[1] != undefined ? rupiah + "," + split[1] : rupiah;
-console.log(rupiah);
-return rupiah;
+  rupiah = split[1] != undefined ? rupiah + "," + split[1] : rupiah;
+  console.log(rupiah);
+  return rupiah;
 }
 
 function convert2number(data) {
-let temp = data.replace(/\./g, ''); // merubah . jadi ""
-return parseFloat(temp);
+  let temp = data.replace(/\./g, ""); // merubah . jadi ""
+  return parseFloat(temp);
 }
 
-export const startSelect2 = function() {
+export const startSelect2 = function () {
   setTimeout(() => {
-    $('.select2').select2();
-  }, 300)
-}
+    $(".select2").select2();
+  }, 300);
+};
 
 /* Tujuan:
 -Inisiasi WYSIWYG menggunakan CKEditor 5 DecoupledEditor
@@ -168,19 +167,22 @@ initEditor = async function (template, options) {
       templateField = options.templateField;
     }
   }
-  DecoupledEditor.create(document.querySelector("#" + editorEl),  {image: {
-    toolbar: [
-      "imageStyle:full",
-      "imageStyle:side",
-      "imageStyle:alignLeft",
-      "imageStyle:alignCenter",
-      "imageStyle:alignRight",
-      "|",
-      "imageStyle:sideLeft",
-      "|",
-      "imageTextAlternative",
-      "toggleImageCaption"
-    ]}})
+  DecoupledEditor.create(document.querySelector("#" + editorEl), {
+    image: {
+      toolbar: [
+        "imageStyle:full",
+        "imageStyle:side",
+        "imageStyle:alignLeft",
+        "imageStyle:alignCenter",
+        "imageStyle:alignRight",
+        "|",
+        "imageStyle:sideLeft",
+        "|",
+        "imageTextAlternative",
+        "toggleImageCaption",
+      ],
+    },
+  })
     .then((editor) => {
       editor.plugins.get("FileRepository").createUploadAdapter = (loader) => {
         return new MyUploadAdapter(loader);
@@ -199,12 +201,12 @@ initEditor = async function (template, options) {
     });
 };
 
-initMultipleEditor = async function (id,template, options) {
+initMultipleEditor = async function (id, template, options) {
   /*     Deskripsi :
     Function ini akan memulai pembuatan WYSIWYG menggunakan Id Elemen yang disediakan
     dan menghasilkan editor yang disimpan di template.editor*/
-  let editorEl = "editor-"+id;
-  let toolbarEl = "toolbar-container-"+id;
+  let editorEl = "editor-" + id;
+  let toolbarEl = "toolbar-container-" + id;
   let content = "";
   let templateField = "editor";
   if (options) {
@@ -222,24 +224,27 @@ initMultipleEditor = async function (id,template, options) {
       templateField = options.templateField;
     }
   }
-  DecoupledEditor.create(document.querySelector("#" + editorEl),  {image: {
-    toolbar: [
-      "imageStyle:full",
-      "imageStyle:side",
-      "imageStyle:alignLeft", 
-      "imageStyle:alignCenter",
-      "imageStyle:alignRight",
-      "|",
-      "imageStyle:sideLeft",
-      "|",
-      "imageTextAlternative",
-      "toggleImageCaption"
-    ]}})
+  DecoupledEditor.create(document.querySelector("#" + editorEl), {
+    image: {
+      toolbar: [
+        "imageStyle:full",
+        "imageStyle:side",
+        "imageStyle:alignLeft",
+        "imageStyle:alignCenter",
+        "imageStyle:alignRight",
+        "|",
+        "imageStyle:sideLeft",
+        "|",
+        "imageTextAlternative",
+        "toggleImageCaption",
+      ],
+    },
+  })
     .then((editor) => {
       editor.plugins.get("FileRepository").createUploadAdapter = (loader) => {
         return new MyUploadAdapter(loader);
       };
-      
+
       // Jika .create-nya selesai maka akan menghasilkan editor di parameternya
       // Ini untuk toolbarnya yang seperti MS Word
       const toolbarContainer = document.querySelector("#" + toolbarEl);
